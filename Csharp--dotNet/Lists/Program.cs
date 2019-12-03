ff0dee23b2950eae6435b6c4ba0c906f0d5cf627
iuc73663 <ivan5247@gmail.com>
2019-12-03 15:58:12 -0800

    added some files

---
+++ Csharp--dotNet/Lists/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> customers = new List<string>();
+            customers.Add("John");
+            customers.Add("Tim");
+            customers.Add("Jason");
+
+            Console.WriteLine(customers.Count);
+            //loop through items 
+            foreach(var customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+            //Specific index 
+            Console.WriteLine(customers[1]);
+        }
+    }
+}
