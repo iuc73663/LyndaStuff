6f90bf9ff9e219c13544d850a64c827840535164
iuc73663 <ivan5247@gmail.com>
2019-12-05 12:37:47 -0800

    more lynda stuff

---
+++ Csharp--dotNet/LearningBitArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningBitArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] preload = new bool[3] {false,true,false};
+            BitArray enemyGrid = new BitArray(preload);
+
+            /*
+            enemyGrid[0] = false;
+            enemyGrid[1] = true;
+            enemyGrid.Set(2, false);
+            */
+            foreach(var i in enemyGrid)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
