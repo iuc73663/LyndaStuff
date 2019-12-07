ceb0bab80662a7f3cceeff2144b6aad403de3cf0
iuc73663 <ivan5247@gmail.com>
2019-12-06 16:11:30 -0800

    more lynda stuff

---
+++ Csharp--dotNet/LearningAsync/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Download.");
+            download();
+            Console.ReadLine();
+        }
+        static void download()
+        {
+            Task.Run(() => {
+                Thread.Sleep(3000);
+                Console.WriteLine("Download complete.");
+            });
+
+        }
+    }
+}
---
+++ Csharp--dotNet/LearningAsyncAwayStart/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsyncAwayStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading file");
+            Download();
+            Console.ReadLine();
+        }
+        static async void Download()
+        {
+            //await Network.Download();
+            HttpClient client = new HttpClient();
+
+            var data = await client.GetStringAsync("http://www.computrition.com/software-products/hospitality-suite/suitepoint-point-of-sale/");
+            Console.WriteLine("Download complete." + data);
+        }
+    }
+    class Network
+    {
+        static public Task Download()
+        {
+            return Task.Run(()=>Thread.Sleep(3000));
+        }
+    }
+}
